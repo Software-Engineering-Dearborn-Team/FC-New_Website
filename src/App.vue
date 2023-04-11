41da75951234f3e289baac20161205d6afd428e0
joeB0dy <sjose@umich.edu>
2023-04-11 15:21:42 -0400

    Somehow working, pushed to a random branch.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
+    <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Farmer's Connect" />
+    
   <div id="app">
-    <img alt="Farmer's Connect logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <nav>
+      <router-link to="/">Home</router-link> |
+    </nav>
+    <router-view/>
   </div>
+  </div>
+  </header>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -28,6 +23,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
