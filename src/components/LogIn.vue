332540f368a30e09d0b50c0f0b1ee516032aa4a8
Sierra Corl <scorl@umich.edu>
2023-04-12 00:09:54 -0400

    changed to template
    changed to vue friendly

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,7 +1,40 @@
-<!DOCTYPE html>
-<html>
+<template>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+
+<h2>Login to Account</h2>
+
+<form action="/action_page.php" method="post">
+  <div class="imgcontainer">
+    <img src="../assets/farm5.jpeg" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+
+</body>
+</template>
+
+
 <style>
 body {font-family: Arial, Helvetica, sans-serif;}
 form {border: 3px solid #588c7e;}
@@ -66,34 +99,3 @@ span.psw {
   }
 }
 </style>
-</head>
-<body>
-
-<h2>Login to Account</h2>
-
-<form action="/action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="images/farm5" alt="Avatar" class="avatar">
-  </div>
-
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-        
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
-
-</body>
-</html>
