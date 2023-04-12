1ab61dba89f5f6cb1d36502d48a1cd0c8ee7b0eb
Yunis2413 <99375649+Yunis2413@users.noreply.github.com>
2023-04-12 00:07:06 -0400

    update on about us

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="about">
-    <body>
-      <h1>Here is a little about us!</h1>
-      <p>
+      <br>
+      <h1 style = "font-family: engraver mt;">
+        About Farmers Connect LLC
+      </h1>
+      
+      <img alt="Farm pic"  src="../assets/farm0.jpeg" height=40% width =40% class="image-spacing">
+      
+      <img alt="Farm pic" src="../assets/farm1.jpeg" height=30% width =32%>
+      <center> 
+        <h3 class = "c" style= "font-family: verdana">
         At Farmer's Connect, we are passionate about helping local farmers connect with their customers in the community. 
         Our main goal is to create a user-friendly platform that makes it easy for customers to find and support their local farmers. 
         Through the Farmer's Connect website, farmers will be able to share information about their products and services, connect with customers, and grow their businesses. 
         Our team is committed to building a web-accessible platform that is accessible to everyone, regardless of their technical ability. 
         We believe that by supporting local farmers, we can help create a more sustainable and equitable food system for everyone.
-      </p>
-   
-
-    </body>
+        </h3>
+      </center>
+      <br>
+      <img alt="Farm pic"  src="../assets/farm2.jpeg" height=38% width =37% class="image-spacing">
+      <img alt="Farm pic"  src="../assets/farm.png" height=40% width =40% >
   </div> 
-
-  <!-- <body>
-   <img src="images/farm.jpeg" style= "width: 100%" alt ="farm"> 
-
-  </body> -->
-  
 </template>
+
+<style> 
+  .image-spacing { 
+    margin-right: 20px; 
+  } 
+
+h3{
+  text-align: center;
+  width: 1100px;
+  border: 1px solid white;
+}
+  h3.c{
+  word-break: break-word;
+}
+</style>
