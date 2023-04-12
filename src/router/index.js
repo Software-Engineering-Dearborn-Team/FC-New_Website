4e6ee26a2ca9ebf3a0ca2891759ca7bbbfea088b
joeB0dy <sjose@umich.edu>
2023-04-11 21:44:53 -0400

    login and sign up added from main into routing

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
+
 const routes = [
   {
     path: '/',
@@ -17,6 +18,9 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+
+
+
   }
 ]
 
