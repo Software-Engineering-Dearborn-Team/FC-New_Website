e3265ff89ef5d9edec2de5ad64d14f51faee79ce
joeB0dy <sjose@umich.edu>
2023-04-11 22:20:01 -0400

    menu organized. need to center and fix stuff

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
     <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
-
+      <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+    
     <div class="wrapper">
       <HelloWorld msg="Farmer's Connect" />
     
   <div id="app">
     
+    <!--navigation for routing links.-->
     <nav>
-      <router-link to="/">Home</router-link> |
+        <ul>
+          <li> <router-link to="/">Home</router-link> </li>
+          <li><router-link to="/about">About Us</router-link></li>
+          <li><router-link to="/login">Log In</router-link>"</li>
+          <li><router-link to="/signup">Sign Up</router-link></li>
+        </ul>
     </nav>
+
     <router-view/>
   </div>
   </div>
@@ -26,15 +33,19 @@
 }
 
 nav {
+  overflow: hidden;
   padding: 30px;
+ 
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: white;
+ 
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
